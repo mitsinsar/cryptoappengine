f8874ae60fa1ad7e749892e73397b8f53f2c4ab3
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2021-03-20 16:02:01 +0300

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import webapp2
+import urllib2
+class UpdateCachedData(webapp2.RequestHandler):
+    def get(self):
+        # request = urllib2.Request('<GCF URL>', headers={"cronrequest" : "true"})
+        # contents = urllib2.urlopen(request).read()
+        print("working every 30 secs")
+app = webapp2.WSGIApplication([
+    ('/update', UpdateCachedData),
+    ], debug=True)
